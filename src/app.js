8c3d50ed22c454ebbd429bf2ee8d98470f5b4165
Aaronsiox <jhoyamp@gmail.com>
2026-04-05 02:41:49 +0000

    Generador de excusas de Aaron

--- src/app.js
+++ src/app.js
@@ -4,8 +4,19 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoRandom = Math.floor(Math.random() * who.length)
+  let actionRandom = Math.floor(Math.random() * action.length)
+  let whatRandom = Math.floor(Math.random() * what.length)
+  let whenRandom = Math.floor(Math.random() * when.length)
+
+  let htmlExcuse = `${who[whoRandom]} ${action[actionRandom]} ${what[whatRandom]} ${when[whenRandom]}`
+  console.log("Hello Rigo from the console!", whoRandom, actionRandom, whatRandom, whenRandom);
+  console.log(htmlExcuse)
+  document.getElementById("excuse").innerHTML = htmlExcuse
 };
